22dc7a493384b556ef2a80840d493102e1dc62c7
CO18307 <amandeep08saini@gmail.com>
2023-11-26 19:42:37 +0530

    validation added on signup and login form with yup and react-hook-form and updated toastr with react-toastify

---
+++ src/Utils/utilities.ts
@@ -0,0 +1,16 @@
+import { object, string } from "yup";
+
+const getCharacterValidationError = (str: string) => {
+	return `Your password must have at least 1 ${str} character`;
+};
+
+export const loginSchema = object({
+	email: string().email().required(),
+	password: string()
+		.required("password is a required field")
+		.matches(/[A-Z]/, getCharacterValidationError("uppercase"))
+		.matches(/[a-z]/, getCharacterValidationError("lowercase"))
+		.matches(/[0-9]/, getCharacterValidationError("digit"))
+		.min(8, "Password must have at least 8 characters")
+		.max(20, "Password must not be greater than 20 characters"),
+}).required();
